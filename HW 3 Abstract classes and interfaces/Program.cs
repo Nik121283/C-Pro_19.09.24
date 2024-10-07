77d26b5a926eb71eaff0dbd5a4004262a9156404
Nikita Gostev <nik121283@gmail.com>
2024-10-07 12:28:04 +0300

    -

--- HW 3 Abstract classes and interfaces/Program.cs
+++ HW 3 Abstract classes and interfaces/Program.cs
@@ -8,10 +8,15 @@ array1.array[0] = 0;
 array1.array[1] = 1;
 array1.array[2] = 2;
 
+Console.WriteLine("");
 array1.Show();
 
 array1.DeleteItem(1);
 
+Console.WriteLine("");
 array1.Show();
 
+array1.AddItem(3);
 
+Console.WriteLine("");
+array1.Show();
