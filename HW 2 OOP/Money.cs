ed5feef6133685cb8e1e654e6943ba69473d370d
Nikita Gostev <nik121283@gmail.com>
2024-10-08 17:40:49 +0300

    Left only Program.cs

--- HW 2 OOP/Money.cs
+++ HW 2 OOP/Money.cs
@@ -10,12 +10,37 @@ namespace HW_2_OOP
     {
         public int dollars;
 
-        public int cents;
+        public float cents;
 
         public Money(float sartingCash)
         {
+            if(sartingCash < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
             dollars = (int)(sartingCash / 1);
-            cents = (int)((sartingCash - dollars)*100);
+            cents = (int)Math.Round((sartingCash - dollars)*100);
+        }
+        public Money(int sartingCashDollars, int sartingCashCents)
+        {
+            if (sartingCashDollars < 0 || sartingCashCents<0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            dollars = sartingCashDollars;
+            cents = sartingCashCents;
+        }
+
+        public void ChangeMoney(float changingSum)
+        {
+            this.dollars = dollars + (int)(changingSum / 1);
+            this.cents = cents + (int)Math.Round(((int)(changingSum / 1) - changingSum) * 100);
+        }
+
+        public string ReturnAccountStatement()
+        {
+            return $"{dollars},{cents}";
         }
 
         public void ShowAccountStatement()
---
+++ HW 2 OOP/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_OOP
+{
+    public class Product
+    {
+        private static int numerator = 0;
+        private int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Money Money { get; set; }
+
+        public Product(string name, string description, float price)
+        {
+            this.Id = numerator;
+            numerator++;
+
+            this.Name = name;
+            this.Description = description;
+            this.Money = new Money(price);
+
+        }
+
+        public Product(string name, string description, int priceDollars, int priceCents)
+        {
+            this.Id = numerator;
+            numerator++;
+
+            this.Name = name;
+            this.Description = description;
+            this.Money = new Money(priceDollars, priceCents);
+        }
+
+
+        public void MakeHigherPrice(float addToPrice)
+        {
+            Money.ChangeMoney(addToPrice);
+        }
+
+        public void MakeLowerPrice(float addToPrice)
+        {
+            Money.ChangeMoney(- addToPrice);
+        }
+
+        public void ShowProductInfo()
+        {
+            Console.WriteLine(this);
+        }
+
+        public override string ToString() { return $"Id:{this.Id} Name:{this.Name} Descr:{this.Description} Price:{this.Money.ReturnAccountStatement()}"; }
+    }
+}
