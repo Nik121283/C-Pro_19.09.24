63cdd9bbf337185902b1ff52b7460165e702601f
Nikita Gostev <nik121283@gmail.com>
2024-10-09 11:24:10 +0300

    Task 3 done

---
+++ HW 2 OOP 3/MyStructure.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_OOP_3
+{
+    public struct MyStructure
+    {
+        public int desytichnoe;
+
+        public MyStructure(int desytichnoe)
+        {
+            this.desytichnoe = desytichnoe;
+        }
+
+        public void convertTo2()
+        {
+            string result = Convert.ToString(desytichnoe, 2);
+            Console.WriteLine(result);
+        }
+
+        public void convertTo8()
+        {
+            string result = Convert.ToString(desytichnoe, 8);
+            Console.WriteLine(result);
+        }
+
+        public void convertTo16()
+        {
+            string result = Convert.ToString(desytichnoe, 16);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ HW 2 OOP 3/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using HW_2_OOP_3;
+
+MyStructure myStructure = new MyStructure(255);
+
+myStructure.convertTo2();
+
+myStructure.convertTo8();
+
+myStructure.convertTo16();
