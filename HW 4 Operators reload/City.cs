57e254577a7c466e72b41757c541d8828e97813a
Nikita Gostev <nik121283@gmail.com>
2024-10-09 16:07:00 +0300

    HW 4 Done Employee and City clasesses

---
+++ HW 4 Operators reload/City.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4_Operators_reload
+{
+    public class City
+    {
+        public string NameOfTheCity { get; set; }
+
+        private int population;
+
+        public int Population 
+        { 
+            get { return population; }
+            set { if (value < 0 || value > 50000000) { throw new ArgumentOutOfRangeException(); } else { population = value; } }
+        }
+
+        public City( string nameOfTheCity, int population)
+        {
+            this.NameOfTheCity = nameOfTheCity;
+            this.Population = population;
+        }
+
+        public override int GetHashCode()
+        {
+            return NameOfTheCity.GetHashCode();  
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj != null) 
+            {
+                if(obj is City city2)
+                {
+                    return this.population == city2.population;
+                }
+            }
+            return false;
+        }
+
+        public static City operator +(City city1, int addingPopulation)
+        {
+            city1.population += addingPopulation;
+            return city1;
+        }
+
+        public static City operator -(City city1, int subtractionPopulation)
+        {
+            city1.population -= subtractionPopulation;
+            return city1;
+        }
+
+        public static bool operator ==(City city1, City city2)
+        {
+            return city1.population == city2.population;
+        }
+
+        public static bool operator !=(City city1, City city2)
+        {
+            return !(city1.population == city2.population);
+        }
+
+
+        public static bool operator >(City city1, City city2)
+        {
+            return (city1.population > city2.population);
+        }
+
+        public static bool operator <(City city1, City city2)
+        {
+            return (city1.population < city2.population);
+        }
+    }
+}
---
+++ HW 4 Operators reload/Employee.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4_Operators_reload
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        private decimal salary;
+
+        public decimal Salary 
+        { 
+            get { return salary; }
+            set
+            {
+                if (value < 0 || value > 100000)
+                {
+                    throw new ArgumentOutOfRangeException("Зарплата должна быть в пределах от 0 до 100 000.");
+                }
+                else
+                {
+                    salary = value;
+                }
+            }
+        }
+
+        public Employee(string name, string surname, decimal salary)
+        {
+            Name = name;
+            Surname = surname;
+            Salary = salary;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Employee otherEmployee)
+            {
+                return this.Salary == otherEmployee.Salary;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Salary.GetHashCode();
+        }
+
+        public static Employee operator +(Employee emp, decimal addingToSalary)
+        {
+            emp.Salary += addingToSalary;
+
+            return emp;
+        }
+
+        public static Employee operator -(Employee emp, decimal addingToSalary)
+        {
+            emp.Salary -= addingToSalary;
+
+            return emp;
+        }
+
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return emp1.Salary==emp2.Salary;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1.Salary == emp2.Salary);
+        }
+
+
+        public static bool operator >(Employee emp1, Employee emp2)
+        {
+            return (emp1.Salary > emp2.Salary);
+        }
+
+        public static bool operator <(Employee emp1, Employee emp2)
+        {
+            return (emp1.Salary < emp2.Salary);
+        }
+
+
+    }
+}
