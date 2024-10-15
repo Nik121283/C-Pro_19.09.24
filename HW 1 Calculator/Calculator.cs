2b150339ee74a04fe70935f715aae7a55704dd0c
Nikita Gostev <nik121283@gmail.com>
2024-10-15 11:02:16 +0300

    HW 1 Calculator done

---
+++ HW 1 Calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_1_Calculator
+{
+    public static class Calculator<T> where T : struct, IComparable<T>
+    {
+        public static T Add(T t1, T t2)
+        {
+            return (dynamic)t1 + (dynamic)t2;
+        }
+
+        public static T Subtract(T t1, T t2)
+        {
+            return (dynamic)t1 - (dynamic)t2;
+        }
+
+        public static T Multiply(T t1, T t2)
+        {
+            return (dynamic)t1 * (dynamic)t2;
+        }
+
+        public static T Divide(T t1, T t2)
+        {
+            if ((dynamic)t1 == null || (dynamic)t2 == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            return (dynamic)t1 / (dynamic)t2;
+        }
+    }
+}
---
+++ HW 1 Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HW_1_Calculator;
+
+Console.WriteLine("Тестим калькулятор");
+
+Console.WriteLine($"\nСкладываем 1 + 2 = {Calculator<int>.Add(1, 2)}");
+
+Console.WriteLine($"\nВычитаем из 5,7 - 2,1 = {Calculator<Double>.Subtract(5.7, 2.1)}");
+
+Console.WriteLine($"\nУмножаем  5,5 Х 10 = {Calculator<Double>.Multiply(5.5, 10)}");
+
+Console.WriteLine($"\nДелим  5,5 / 2 = {Calculator<Double>.Divide(5.5,2)}");
