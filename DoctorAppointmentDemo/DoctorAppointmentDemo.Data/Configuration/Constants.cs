0bcaebb5f2153934abd8f9eb858002725d6f2b75
Nikita Gostev <nik121283@gmail.com>
2024-10-18 18:20:19 +0300

    Changed Path

---
+++ DoctorAppointmentDemo/DoctorAppointmentDemo.Data/Configuration/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace MyDoctorAppointment.Data.Configuration
+{
+    public static class Constants
+    {
+        // заменить на путь валидный для вашей директории на пк (в будущем будем использовать относительный путь)
+        public const string AppSettingsPath = "E:\\Обучение\\C#\\C# Hillel Pro 09.09.24\\DoctorAppointmentDemo\\DoctorAppointmentDemo.Data\\Configuration\\appsettings.json";
+    }
+}
