44508e4b247d975e30235945f24843444d6ca2bc
Nikita Gostev <nik121283@gmail.com>
2024-12-18 15:04:49 +0200

    start mvc Note

---
+++ HW11/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HW11.Models
+{
+    public class Note
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        public string NotesText { get; set; }
+
+        [Column(TypeName = "Date")]
+        public DateOnly NoteDateOnly { get; set; }
+    }
+}
